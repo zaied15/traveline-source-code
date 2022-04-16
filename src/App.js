361ec50414c3d2edbe8d49639ac7b0cbd54c1b3c
unknown <mda800@gmail.com>
2022-04-16 22:06:42 +0600

    all dependency installed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Home/Home";
+import About from "./Pages/About/About";
+import Blog from "./Pages/Blog/Blog";
+import NotFound from "./Pages/NotFound/NotFound";
+import Header from "./Pages/Shared/Header/Header";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/blog" element={<Blog></Blog>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Banner = () => {
+  return <div>This is banner</div>;
+};
+
+export default Banner;
