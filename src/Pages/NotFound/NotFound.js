10bbddc3135231d07acad8ccdf34794344c9f84b
unknown <mda800@gmail.com>
2022-04-17 18:05:07 +0600

    404 page completed

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { Container, Row } from "react-bootstrap";
+import notFoundImg from "../../images/404.jpg";
 
 const NotFound = () => {
   return (
     <div>
-      <h2>Page Not Found</h2>
+      <Container>
+        <Row className="d-flex justify-content-center">
+          <img className="w-75 mt-5 mb-2" src={notFoundImg} alt="" />
+        </Row>
+      </Container>
     </div>
   );
 };
